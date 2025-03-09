32ec02213c417045f29aea49579b0aa6dd93af23
Pramodya <pramodya24senevirathna2001@gmail.com>
2025-03-09 19:43:39 +0530

    Make changes in BookingController

--- src/main/java/com/System/MegaCity/controllers/BookingController.java
+++ src/main/java/com/System/MegaCity/controllers/BookingController.java
@@ -28,97 +28,102 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @CrossOrigin(origins = "*")
-@RequestMapping(value = "/booking")
+@RequestMapping(value = "/bookings")
 @Slf4j
 public class BookingController {
 
-    @Autowired
-    private BookingService bookingService;
+        @Autowired
+        private BookingService bookingService;
 
-    @Autowired
-    private CustomerRepository customerRepository;
+        @Autowired
+        private CustomerRepository customerRepository;
 
-    @PostMapping("/createbooking")
-    public ResponseEntity<Booking> createBooking(
-            @AuthenticationPrincipal UserDetails userDetails,
-            @Validated @RequestBody BookingRequestDTO bookingRequest) {
+        @PostMapping("/createbooking")
+        public ResponseEntity<Booking> createBooking(
+                        @AuthenticationPrincipal UserDetails userDetails,
+                        @Validated @RequestBody BookingRequestDTO bookingRequest) {
 
-        String email = userDetails.getUsername();
-        log.info("Create new booking for customer email: {}", email);
+                String email = userDetails.getUsername();
+                log.info("Create new booking for customer email: {}", email);
 
-        Customer customer = customerRepository.findByEmail(email)
-                .orElseThrow(() -> new ResourceNotFoundException("Customer not found with email:" + email));
+                Customer customer = customerRepository.findByEmail(email)
+                                .orElseThrow(() -> new ResourceNotFoundException(
+                                                "Customer not found with email:" + email));
 
-        bookingRequest.setCustomerId(customer.getCustomerId());
-        Booking booking = bookingService.createBooking(bookingRequest);
+                bookingRequest.setCustomerId(customer.getCustomerId());
+                Booking booking = bookingService.createBooking(bookingRequest);
 
-        return ResponseEntity.ok(booking);
-    }
+                return ResponseEntity.ok(booking);
+        }
 
-    @PostMapping("/{bookingId}/cancel")
-    public ResponseEntity<Booking> cancelBooking(
-            @AuthenticationPrincipal UserDetails userDetails,
-            @PathVariable String bookingId,
-            @Validated @RequestBody CancellationRequest cancellationRequest) {
-        String email = userDetails.getUsername();
-        log.info("Cancelling booking: {} for customer email : {} ", bookingId, email);
+        @PostMapping("/{bookingId}/cancel")
+        public ResponseEntity<Booking> cancelBooking(
+                        @AuthenticationPrincipal UserDetails userDetails,
+                        @PathVariable String bookingId,
+                        @Validated @RequestBody CancellationRequest cancellationRequest) {
+                String email = userDetails.getUsername();
+                log.info("Cancelling booking: {} for customer email : {} ", bookingId, email);
 
-        Customer customer = customerRepository.findByEmail(email)
-                .orElseThrow(() -> new ResourceNotFoundException("Customer not found with email:" + email));
+                Customer customer = customerRepository.findByEmail(email)
+                                .orElseThrow(() -> new ResourceNotFoundException(
+                                                "Customer not found with email:" + email));
 
-        cancellationRequest.setBookingId(bookingId);
-        Booking cancelledBooking = bookingService.cancelBooking(customer.getCustomerId(), cancellationRequest);
-        return ResponseEntity.ok(cancelledBooking);
+                cancellationRequest.setBookingId(bookingId);
+                Booking cancelledBooking = bookingService.cancelBooking(customer.getCustomerId(), cancellationRequest);
+                return ResponseEntity.ok(cancelledBooking);
 
-    }
+        }
 
-    @GetMapping("/getallBookings")
-    public ResponseEntity<List<Booking>> getCustomerBookings(
-            @AuthenticationPrincipal UserDetails userDetails) {
+        @GetMapping("/getallBookings")
+        public ResponseEntity<List<Booking>> getCustomerBookings(
+                        @AuthenticationPrincipal UserDetails userDetails) {
 
-        String email = userDetails.getUsername();
-        log.info("Fetching bookings for customer email {}", email);
+                String email = userDetails.getUsername();
+                log.info("Fetching bookings for customer email {}", email);
 
-        Customer customer = customerRepository.findByEmail(email)
-                .orElseThrow(() -> new ResourceNotFoundException("Customer not found with email:" + email));
+                Customer customer = customerRepository.findByEmail(email)
+                                .orElseThrow(() -> new ResourceNotFoundException(
+                                                "Customer not found with email:" + email));
 
-        List<Booking> bookings = bookingService.getCustomerBookings(customer.getCustomerId());
-        return ResponseEntity.ok(bookings);
+                List<Booking> bookings = bookingService.getCustomerBookings(customer.getCustomerId());
+                return ResponseEntity.ok(bookings);
 
-    }
+        }
 
-    @GetMapping("/{bookingId}")
-    public ResponseEntity<Booking> getCustomerBooking(
-            @AuthenticationPrincipal UserDetails userDetails,
-            @PathVariable String bookingId) {
+        @GetMapping("/{bookingId}")
+        public ResponseEntity<Booking> getCustomerBooking(
+                        @AuthenticationPrincipal UserDetails userDetails,
+                        @PathVariable String bookingId) {
 
-        String email = userDetails.getUsername();
-        log.info("fetching booking: {} for customer email: {}", bookingId, email);
+                String email = userDetails.getUsername();
+                log.info("fetching booking: {} for customer email: {}", bookingId, email);
 
-        Customer customer = customerRepository.findByEmail(email)
-                .orElseThrow(() -> new ResourceNotFoundException("Customer not found with email :" + email));
+                Customer customer = customerRepository.findByEmail(email)
+                                .orElseThrow(() -> new ResourceNotFoundException(
+                                                "Customer not found with email :" + email));
 
-        Booking booking = bookingService.getBookingDetails(customer.getCustomerId(), bookingId);
+                Booking booking = bookingService.getBookingDetails(customer.getCustomerId(), bookingId);
 
-        return ResponseEntity.ok(booking);
+                return ResponseEntity.ok(booking);
 
-    }
+        }
 
-    @DeleteMapping("/delete/{bookingId}")
-    public ResponseEntity<Void> deleteBooking(
+        @DeleteMapping("/delete/{bookingId}")
+        public ResponseEntity<Void> deleteBooking(
 
-            @AuthenticationPrincipal UserDetails userDetails,
-            @PathVariable String bookingId) {
+                        @AuthenticationPrincipal UserDetails userDetails,
+                        @PathVariable String bookingId) {
 
-        String email = userDetails.getUsername();
-        log.info("Deleting booking: {} for customer email: {} ", bookingId, email);
+                String email = userDetails.getUsername();
+                log.info("Deleting booking: {} for customer email: {} ", bookingId, email);
 
-        Customer customer = customerRepository.findByEmail(email)
-                .orElseThrow(() -> new ResourceNotFoundException("Customer not found with email :" + email));
+                Customer customer = customerRepository.findByEmail(email)
+                                .orElseThrow(() -> new ResourceNotFoundException(
+                                                "Customer not found with email :" + email));
 
-        bookingService.deleteBooking(customer.getCustomerId(), bookingId);
-        return ResponseEntity.noContent().build();
+                bookingService.deleteBooking(customer.getCustomerId(), bookingId);
+                return ResponseEntity.noContent().build();
 
-    }
+        }
 
 }
